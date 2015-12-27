77a6a15c60a0f79a40e71649f34cc6f18b46f1fe
Philip Damra <djheru@gmail.com>
2015-12-27 17:23:15 -0500

    Adding toggling functionality to the reducer

--- src/todo-reducer.js
+++ src/todo-reducer.js
@@ -13,6 +13,13 @@ const todos = (state = [], action = {type: ''}) => {
           completed: false
         }
       ];
+
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        return (todo.id === action.id)
+          ? Object.assign({}, todo, {completed: !todo.completed}) : todo;
+      });
+
     default:
       return state;
   }
@@ -40,5 +47,28 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {id: 0, text: 'Learn Redux', completed: false},
+    {id: 1, text: 'Go Shopping', completed: false}
+  ];
+  const stateAfter = [
+    {id: 0, text: 'Learn Redux', completed: false},
+    {id: 1, text: 'Go Shopping', completed: true}
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('all tests pass');
